903bd88b855e9d5b757748b3c369479f86620dbe
Darek Mihocka <softmac@outlook.com>
2025-12-08 03:00:56 -0400

    validate MADD VAES VPCLMUL and some missing data movement

--- test_intrins/intrin-list.h
+++ test_intrins/intrin-list.h
@@ -58,6 +58,9 @@ DEFINE_TEST_OP_RAB (_mm_hadds_epi16,        __m128i,    __m128i,    __m128i)
 DEFINE_TEST_OP_RAB (_mm_hadd_pd,            __m128d,    __m128d,    __m128d)
 DEFINE_TEST_OP_RAB (_mm_hadd_ps,            __m128,     __m128,     __m128)
 
+DEFINE_TEST_OP_RAB (_mm_madd_epi16,         __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_maddubs_epi16,      __m128i,    __m128i,    __m128i)
+
 DEFINE_TEST_OP_RAB (_mm_and_si128,          __m128i,    __m128i,    __m128i)
 DEFINE_TEST_OP_RAB (_mm_and_pd,             __m128d,    __m128d,    __m128d)
 DEFINE_TEST_OP_RAB (_mm_and_ps,             __m128,     __m128,     __m128)
@@ -266,6 +269,18 @@ DEFINE_TEST_OP_R   (_mm_undefined_pd,       __m128d)
 DEFINE_TEST_OP_R   (_mm_undefined_ps,       __m128)
 DEFINE_TEST_OP_R   (_mm_undefined_si128,    __m128i)
 
+// Westmere 128-bit AES
+
+DEFINE_TEST_OP_RAB (_mm_aesdec_si128,       __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_aesdeclast_si128,   __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_aesenc_si128,       __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_aesenclast_si128,   __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RA  (_mm_aesimc_si128,       __m128i,    __m128i)
+DEFINE_TEST_OP_RAI (_mm_aeskeygenassist_si128, __m128i,    __m128i,    0xFF)
+
+DEFINE_TEST_OP_RABI(_mm_clmulepi64_si128,   __m128i,    __m128i,    __m128i,    0x00)
+DEFINE_TEST_OP_RABI(_mm_clmulepi64_si128,   __m128i,    __m128i,    __m128i,    0x11)
+
 #if defined(__AVX2__) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 2))
 
 // Arithmetic AVX/AVX2
@@ -309,6 +324,9 @@ DEFINE_TEST_OP_RAB (_mm256_hadds_epi16,     __m256i,    __m256i,    __m256i)
 
 DEFINE_TEST_OP_RAB (_mm256_hadd_pd,         __m256d,    __m256d,    __m256d)
 DEFINE_TEST_OP_RAB (_mm256_hadd_ps,         __m256,     __m256,     __m256)
+
+DEFINE_TEST_OP_RAB (_mm256_madd_epi16,      __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RAB (_mm256_maddubs_epi16,   __m256i,    __m256i,    __m256i)
 
 DEFINE_TEST_OP_RAB (_mm256_and_si256,       __m256i,    __m256i,    __m256i)
 DEFINE_TEST_OP_RAB (_mm256_and_pd,          __m256d,    __m256d,    __m256d)
@@ -471,6 +489,20 @@ DEFINE_TEST_OP_RAI (_mm256_srli_si256,      __m256i,    __m256i,    31)
 
 DEFINE_TEST_OP_RA  (_mm256_castsi256_si128, __m128i,    __m256i)
 
+DEFINE_TEST_OP_RAI (_mm256_extractf128_pd,  __m128d,    __m256d,    0)
+DEFINE_TEST_OP_RAI (_mm256_extractf128_pd,  __m128d,    __m256d,    1)
+DEFINE_TEST_OP_RAI (_mm256_extractf128_ps,  __m128,     __m256,     0)
+DEFINE_TEST_OP_RAI (_mm256_extractf128_ps,  __m128,     __m256,     1)
+DEFINE_TEST_OP_RAI (_mm256_extracti128_si256, __m128i,  __m256i,    0)
+DEFINE_TEST_OP_RAI (_mm256_extracti128_si256, __m128i,  __m256i,    1)
+
+DEFINE_TEST_OP_RABI(_mm256_insertf128_pd,   __m256d,    __m256d,    __m128d,     0)
+DEFINE_TEST_OP_RABI(_mm256_insertf128_pd,   __m256d,    __m256d,    __m128d,     1)
+DEFINE_TEST_OP_RABI(_mm256_insertf128_ps,   __m256,     __m256,     __m128,      0)
+DEFINE_TEST_OP_RABI(_mm256_insertf128_ps,   __m256,     __m256,     __m128,      1)
+DEFINE_TEST_OP_RABI(_mm256_inserti128_si256, __m256i,   __m256i,    __m128i,     0)
+DEFINE_TEST_OP_RABI(_mm256_inserti128_si256, __m256i,   __m256i,    __m128i,     1)
+
 DEFINE_TEST_OP_RABI(_mm256_permute2f128_pd, __m256d,    __m256d,    __m256d,     0)
 DEFINE_TEST_OP_RABI(_mm256_permute2f128_pd, __m256d,    __m256d,    __m256d,     1)
 
@@ -507,9 +539,62 @@ DEFINE_TEST_OP_VAB (_mm256_store_ps,                    pfloat,     __m256)
 DEFINE_TEST_OP_VAB (_mm256_storeu_pd,                   pdouble,    __m256d)
 DEFINE_TEST_OP_VAB (_mm256_storeu_ps,                   pfloat,     __m256)
 
-#if defined(__AVX2512F__) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 3))
+#endif // AVX2 tests
 
-// Post-AVX2 (not supported by Prism or Rosetta at this time April 2025)
+//
+// Post-AVX2 extensions to SSE and AVX since Haswell
+//
+
+#if (defined(__AVX2__) && defined(__VAES__)) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 2))
+
+// 256-bit AES (introduced in Zen2, Ice Lake?)
+
+DEFINE_TEST_OP_RAB (_mm256_aesdec_epi128,    __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RAB (_mm256_aesdeclast_epi128,__m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RAB (_mm256_aesenc_epi128,    __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RAB (_mm256_aesenclast_epi128,__m256i,    __m256i,    __m256i)
+
+DEFINE_TEST_OP_RABI(_mm256_clmulepi64_epi128,__m256i,    __m256i,    __m256i,   0x00)
+DEFINE_TEST_OP_RABI(_mm256_clmulepi64_epi128,__m256i,    __m256i,    __m256i,   0x11)
+
+#endif // AVX2 VAES tests
+
+#if (defined(__AVX2__) && defined(__SHANI__)) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 3))
+
+// 128-bit SHA (introduced in Zen2, Pentium Silver)
+
+DEFINE_TEST_OP_RAB (_mm_sha1msg1_epu32,     __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_sha1msg2_epu32,     __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_sha1nexte_epu32,    __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RABI(_mm_sha1rnds4_epu32,    __m128i,    __m128i,    __m128i,    0)
+DEFINE_TEST_OP_RABI(_mm_sha1rnds4_epu32,    __m128i,    __m128i,    __m128i,    1)
+DEFINE_TEST_OP_RABI(_mm_sha1rnds4_epu32,    __m128i,    __m128i,    __m128i,    2)
+DEFINE_TEST_OP_RABI(_mm_sha1rnds4_epu32,    __m128i,    __m128i,    __m128i,    3)
+DEFINE_TEST_OP_RAB (_mm_sha256msg1_epu32,   __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RAB (_mm_sha256msg2_epu32,   __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RABC(_mm_sha256rnds2_epu32,  __m128i,    __m128i,    __m128i,    __m128i)
+
+#endif // SHANI tests
+
+#if (defined(__AVX2__) && defined(__AVX_VNNI__)) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 3))
+
+// Alder Lake AVX-VNNI
+
+DEFINE_TEST_OP_RABC(_mm_dpbusd_avx_epi32,   __m128i,    __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RABC(_mm_dpbusds_avx_epi32,  __m128i,    __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RABC(_mm_dpwssd_avx_epi32,   __m128i,    __m128i,    __m128i,    __m128i)
+DEFINE_TEST_OP_RABC(_mm_dpwssds_avx_epi32,  __m128i,    __m128i,    __m128i,    __m128i)
+
+DEFINE_TEST_OP_RABC(_mm256_dpbusd_avx_epi32, __m256i,   __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RABC(_mm256_dpbusds_avx_epi32,__m256i,   __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RABC(_mm256_dpwssd_avx_epi32, __m256i,   __m256i,    __m256i,    __m256i)
+DEFINE_TEST_OP_RABC(_mm256_dpwssds_avx_epi32,__m256i,   __m256i,    __m256i,    __m256i)
+
+#endif
+
+#if defined(__AVX512F__) || (defined(USE_SOFT_INTRINSICS) && (USE_SOFT_INTRINSICS >= 3))
+
+// AVX-512 packed integer extensions which complete the original SSE2 and AVX2 opcodes
 
 DEFINE_TEST_OP_RA  (_mm_abs_epi64,          __m128i,    __m128i)
 
@@ -520,6 +605,4 @@ DEFINE_TEST_OP_RAB (_mm256_srlv_epi16,      __m256i,    __m256i,    __m256i)
 DEFINE_TEST_OP_RAB (_mm256_srav_epi16,      __m256i,    __m256i,    __m256i)
 
 #endif // AVX-512 tests
-
-#endif // AVX2 tests
 
